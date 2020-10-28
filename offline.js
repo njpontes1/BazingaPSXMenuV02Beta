4658ec2baca115ae95319a99677df7f6a1faca75
njpontes1 <69497131+njpontes1@users.noreply.github.com>
2020-10-28 00:53:55 +0000

    .
    .

---
+++ offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1603845865,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/sprite-sheet1.png",
+		"images/l-sheet0.png",
+		"images/r-sheet0.png",
+		"images/info-sheet0.png",
+		"images/fundo-sheet0.png",
+		"images/ps1-sheet0.png",
+		"images/ps1-sheet1.png",
+		"images/ps1-sheet2.png",
+		"images/button-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
